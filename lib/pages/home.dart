6a23ff3d6b9df915134893ea1e7a47b5cb1372d1
Rodolfo Schmitt <Rodo-Hans@Hotmail.com>
2019-11-14 07:43:54 -0600

    Added a row of images and a container with box decoration.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -32,7 +32,30 @@ class _HomeState extends State<Home> {
           ],
         ),
         body: SingleChildScrollView(
-          child: Column(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                const ImagesAndIconWidget(),
+                Divider(),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.orange,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 10.0,
+                        offset: Offset(0.0, 10.0),
+                      )
+                    ]
+                  ),
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
@@ -40,3 +63,33 @@ class _HomeState extends State<Home> {
   }
 }
 
+class ImagesAndIconWidget extends StatelessWidget {
+  const ImagesAndIconWidget({
+    Key key
+}): super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Image(
+          image: AssetImage('assets/images/house.png'),
+          fit: BoxFit.cover,
+          color: Colors.blue,
+          width: MediaQuery.of(context).size.width / 3,
+          height: MediaQuery.of(context).size.width / 3,
+        ),
+        Image.network(
+          'https://flutter.io/images/catalog-widget-placeholder.png',
+          width: MediaQuery.of(context).size.width / 3,
+        ),
+        Icon(
+          Icons.brush,
+          color: Colors.lightBlue,
+          size: MediaQuery.of(context).size.width / 4,
+        )
+      ],
+    );
+  }
+}
